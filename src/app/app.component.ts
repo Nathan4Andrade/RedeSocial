3710959f0418291665f6c36128657b234005ad73
Nathan Andrade <nathanandrade20@gmail.com>
2018-02-07 12:05:32 -0200

    add-jsonserver

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-social',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent  implements OnInit{
+  name = 'Angular 5';
+  api = 'https://my-json-server.typicode.com/gugawag/app-social/usuarios';
+
+  usuarios = [];
+  constructor(private http: HttpClient) {
+
+  }
+  
+  
+  ngOnInit() {
+    this.http.get<any[]>(this.api).subscribe(
+      usuarios => {
+        this.usuarios = usuarios;
+      }
+    );
+  }
+
+  remover(usuario) {
+    this.http.delete(`${this.api}/${usuario.id}`).subscribe(
+      sucesso => {
+        console.log('Apagou');
+        this.usuarios.splice(usuario.id, 1);
+      },
+      erro => {console.log('Erro ao apagar: ' + erro)}
+    );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,14 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import {AppComponent} from './app.component';
-import {MatInputModule, MatButtonModule, MatCheckboxModule, MatCardModule} from '@angular/material';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
 import {HttpClientModule} from '@angular/common/http';
-import {AppRoutingModuleModule} from './app-routing.module';
-import {UsuarioModule} from './usuario/usuario.module';
-import {MensagemModule} from './mensagem/mensagem.module';
-import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule, FormsModule,
-    MatInputModule, MatButtonModule, MatCheckboxModule, MatCardModule,
-    UsuarioModule,
-    MensagemModule,
-    AppRoutingModuleModule,
-    RouterModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
